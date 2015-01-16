e0360a828cf59d7e056fd7322c4b9d9989f52466
Ian Merrick <MerrickI@cf.ac.uk>
2015-01-16 23:27:06 +0000

    Added multiple rotations - not sure these are correct yet; COnverted the plotting routine from matplotlib to mayavi - matplotlib assumed equal scales for all dimensions on 3d projections, which wasn't the case for this code, so it was difficult to interpret graphically - mayavi fixes this issue.

--- src/py_ellipsoids.py
+++ src/py_ellipsoids.py
@@ -1,11 +1,12 @@
 #!/bin/python
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
+#from mpl_toolkits.mplot3d import Axes3D
 import math
+from mayavi import mlab
 
-
-def rotate_about_xaxis(angle, point):
-    """Returns a point rotated by angle radians about the x axis.
+def rotate_about_xaxis(alpha, point):
+    """Returns a point rotated by alpha radians about the x axis.
     
     """
     Rx=np.array([[             1,                0,               0 ],
@@ -15,8 +16,8 @@ def rotate_about_xaxis(angle, point):
     return np.dot(Rx,np.transpose(point));
 
 
-def rotate_about_yaxis(angle, point):
-    """Returns a point rotated by angle radians about the y axis.
+def rotate_about_yaxis(beta, point):
+    """Returns a point rotated by beta radians about the y axis.
     
     """
     Ry=np.array([[  np.cos(beta),              0, -np.sin(beta) ],
@@ -26,8 +27,8 @@ def rotate_about_yaxis(angle, point):
     return np.dot(Ry,np.transpose(point));
     
     
-def rotate_about_zaxis(angle, point):
-    """Returns a point rotated by angle radians about the z axis.
+def rotate_about_zaxis(gamma, point):
+    """Returns a point rotated by gamma radians about the z axis.
     
     """
     Rz=np.array([[ np.cos(gamma),  np.sin(gamma),             0 ],
@@ -37,16 +38,24 @@ def rotate_about_zaxis(angle, point):
     return np.dot(Rz,np.transpose(point));
     
     
-def rotate_about_xyz(angleX, angleY, angleZ, point):
-    """Returns a point rotated by angleX, angleY, and angleZ radians about the
-       x-, y-, and z-axis respectively.
+def rotate_about_u(theta, point, u):
+    """Returns a point rotated by angle about vector u.
     
-    """
-    Rz=np.array([[ np.cos(gamma),  np.sin(gamma),             0 ],
-                 [-np.sin(gamma),  np.cos(gamma),             0 ],
-                 [             0,              0,             1 ]])
+    """ 
+    c=math.cos(theta)
+    s=math.sin(theta)
+    ux=u[0]
+    uy=u[1]
+    uz=u[2]
+    ux2=u[0]*u[0]
+    uy2=u[1]*u[1]
+    uz2=u[2]*u[2]
+
+    R=np.array([[ c+ux2*(1.-c)      ,  ux*uy*(1.-c) -uz*s, ux*uz*(1.-c)+uy*s ],
+                [ uy*ux*(1-c) + uz*s,  c+uy2*(1.-c)      , uy*uz*(1.-c)-ux*s ],
+                [ uz*ux*(1.-c)-uy*s ,  uz*uy*(1.-c)+ux*s , c+uz2*(1.-c)      ]])
     
-    return np.dot(Rz,np.transpose(point));
+    return np.dot(R,np.transpose(point));
 
 
 ########################################
@@ -57,7 +66,7 @@ def rotate_about_xyz(angleX, angleY, angleZ, point):
 #    ----   +  ----   +  ----    =   1
 #    a**2      b**2      c**2
 #======================================
-coefs = (5., 3., 1.)
+coefs = (10., 3., 1.)
 
 ngrid=120
 
@@ -81,24 +90,51 @@ yr3=np.zeros((120,120))
 zr3=np.zeros((120,120))
 
 
-alpha=np.pi/6
-beta=np.pi/4
-gamma=np.pi/2
+#alpha=np.pi/6  # 30 degrees 
+#beta =np.pi/4   # 45 degrees 
+#gamma=np.pi/6  # 30 degrees 
+
+alpha=np.pi/2  # 90 degrees 
+beta =np.pi/2  # 90 degrees 
+gamma=np.pi/2  # 90 degrees 
     
 # Rotate about x-axis, rotated points now stored in [xyz]r1
 # Perform rotation on individual points about X-axis
+u=np.array([1.,0.,0.])
 for i in range(ngrid):
   for j in range(ngrid):
-    (xr1[i,j],yr1[i,j],zr1[i,j]) = rotate_about_xaxis(alpha,([x[i,j],y[i,j],z[i,j]]));
+    (xr1[i,j],yr1[i,j],zr1[i,j]) = rotate_about_u(-1.*alpha,([x[i,j],y[i,j],z[i,j]]),u);
 
-                        
-# plot
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-#ax.plot_wireframe(x, y, z, rstride=4, cstride=4, color='k', alpha=0.25)
-ax.plot_wireframe(
-                  xr1, yr1, zr1, rstride=8, cstride=8, color='k', alpha=0.25)
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')
+
+# Rotate about object's x-axis, rotated points now stored in [xyz]r2
+# Perform rotation on individual points about u
+u=np.array([math.cos(alpha),(-1)*math.sin(alpha),0.])
+for i in range(ngrid):
+  for j in range(ngrid):
+    (xr2[i,j],yr2[i,j],zr2[i,j]) = rotate_about_u(-1.*beta,([xr1[i,j],yr1[i,j],zr1[i,j]]),u);
+
+       
+# Rotate about object's x-axis, rotated points now stored in [xyz]r2
+# Perform rotation on individual points about u
+u=np.array([math.sin(alpha)*math.cos(beta),
+            math.cos(alpha)*math.cos(beta),
+           (-1.)*math.sin(beta)*math.cos(alpha)])
+for i in range(ngrid):
+  for j in range(ngrid):
+    (xr3[i,j],yr3[i,j],zr3[i,j]) = rotate_about_u(gamma,([xr2[i,j],yr2[i,j],zr2[i,j]]),u);
+                 
+       
+#m = mlab.mesh(x  , y  , z  , color=(0.0, 0.0, 1.0))  # blue
+#n = mlab.mesh(xr1, yr1, zr1, color=(0.0, 1.0, 0.0))  # green
+#o = mlab.mesh(xr2, yr2, zr2, color=(1.0, 0.0, 0.0))  # red
+p = mlab.mesh(xr3, yr3, zr3, color=(1.0, 1.0, 0.0))  # yellow
+mlab.show()
+                 
+# plot with matplotlib
+#fig = plt.figure()
+#ax = fig.add_subplot(111, projection='3d')
+#ax.plot_wireframe(xr3, yr3, zr3, rstride=8, cstride=8, color='k', alpha=0.25)
+#ax.set_xlabel('x')
+#ax.set_ylabel('y')
+#ax.set_zlabel('z')
 #plt.show()
