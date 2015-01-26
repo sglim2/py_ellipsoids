8c23f3ba57bc6582ccd7c99cdc535cf85f4d5d5c
Ian Merrick <MerrickI@cf.ac.uk>
2015-01-26 11:37:34 +0000

    Created an icosahedron class, which is now used in place of the module file.

--- src/py_ellipsoids.py
+++ src/py_ellipsoids.py
@@ -4,7 +4,7 @@ import math
 import icosahedron as ico
 from collada import *
 import ConfigParser
-
+from icosahedron2 import Icosahedron2 
 
 def parse_config():
     configfile=open('ellipsoids.cfg', 'r')
@@ -16,18 +16,19 @@ def parse_config():
     params=header.split()
 
     name={}
+    key=0
     for line in lines[1:]:
         if line == '\n':
             continue
         words = line.split()
-        name = words[0]
+        name[key] = words[0]
         values = words[1:]
-        data[name]={}
+        data[name[key]]={}
         for p, v in zip(params, values):
             if v != 'n/a':
-                data[name][p] = float(v)
-
-    return data
+                data[name[key]][p] = float(v)
+        key += 1
+    return name,data
             
             
 
@@ -79,73 +80,16 @@ def merge_collada_objects(list_collada_objects):
     merged_collada_object.scene = myscene       
            
     return merged_collada_object
-    
-def rotate_point_about_xaxis(alpha, point):
-    """Returns a point rotated by alpha radians about the x axis.
-    
-    """
-    Rx=np.array([[             1,                0,               0 ],
-                 [             0,  math.cos(alpha), math.sin(alpha) ],
-                 [             0, -math.sin(alpha), math.cos(alpha) ]])
-    
-    return np.dot(Rx,np.transpose(point));
+ 
+ 
 
-
-def rotate_point_about_yaxis(beta, point):
-    """Returns a point rotated by beta radians about the y axis.
-    
-    """
-    Ry=np.array([[  np.cos(beta),              0, -np.sin(beta) ],
-                 [             0,              1,             0 ],
-                 [  np.sin(beta),              0,  np.cos(beta) ]])
-    
-    return np.dot(Ry,np.transpose(point));
-    
-    
-def rotate_point_about_zaxis(gamma, point):
-    """Returns a point rotated by gamma radians about the z axis.
-    
-    """
-    Rz=np.array([[ np.cos(gamma),  np.sin(gamma),             0 ],
-                 [-np.sin(gamma),  np.cos(gamma),             0 ],
-                 [             0,              0,             1 ]])
-    
-    return np.dot(Rz,np.transpose(point));
-    
-    
-def rotate_point_about_u(theta, point, u):
-    """Returns a point rotated by angle about vector u.
-    
-    """ 
-    c=math.cos(theta)
-    s=math.sin(theta)
-    ux=u[0]
-    uy=u[1]
-    uz=u[2]
-    ux2=u[0]*u[0]
-    uy2=u[1]*u[1]
-    uz2=u[2]*u[2]
-
-    R=np.array([[ c+ux2*(1.-c)      ,  ux*uy*(1.-c) -uz*s, ux*uz*(1.-c)+uy*s ],
-                [ uy*ux*(1-c) + uz*s,  c+uy2*(1.-c)      , uy*uz*(1.-c)-ux*s ],
-                [ uz*ux*(1.-c)-uy*s ,  uz*uy*(1.-c)+ux*s , c+uz2*(1.-c)      ]])
-    
-    return np.dot(R,np.transpose(point));
-
-
-
-data=parse_config()
+(name,data)=parse_config()
 # Google-Earth has a limits of 64k vertices
-(TP,NP,indices,NT) = ico.create_icosahedron_optimized(8)
+TP=NP=indices=Ellipsoid={}
+#(TP,NP,indices,NT) = ico.create_icosahedron_optimized(24)
 
-########################################
-########################################
-    
-# build coefficients ==================
-#    x**2      y**2      z**2
-#    ----   +  ----   +  ----    =   1
-#    a**2      b**2      c**2
-#======================================
+Ellipsoid=Icosahedron2(24)
+
 coeffs=np.array((25.,50.,100.))
 
 # Define rotations
@@ -154,47 +98,28 @@ beta =np.pi/4  # 90 degrees
 gamma=np.pi/3  # 90 degrees 
     
 # Scale to ellipsoid
-TP=ico.stretch(TP,coeffs[0],coeffs[1],coeffs[2])
+#TP=ico.stretch(TP,coeffs[0],coeffs[1],coeffs[2])
+Ellipsoid.stretch(coeffs[0],coeffs[1],coeffs[2])
 
-# Scale to ellipsoid and normals
+
 u=np.array([1.,0.,0.])
-TP=ico.rotate_about_u(TP,alpha,u)
-NP=ico.rotate_about_u(NP,alpha,u)
+#TP=ico.rotate_about_u(TP,alpha,u)
+#NP=ico.rotate_about_u(NP,alpha,u)
+Ellipsoid.rotate_about_u(alpha,u)
 
 u=np.array([math.cos(alpha),(-1)*math.sin(alpha),0.])
-TP=ico.rotate_about_u(TP,beta,u)
-NP=ico.rotate_about_u(NP,beta,u)
+#TP=ico.rotate_about_u(TP,beta,u)
+#NP=ico.rotate_about_u(NP,beta,u)
+Ellipsoid.rotate_about_u(beta,u)
 
 u=np.array([math.sin(alpha)*math.cos(beta),
             math.cos(alpha)*math.cos(beta),
            (-1.)*math.sin(beta)*math.cos(alpha)])
-TP=ico.rotate_about_u(TP,gamma,u)
-NP=ico.rotate_about_u(NP,gamma,u)
+#TP=ico.rotate_about_u(TP,gamma,u)
+#NP=ico.rotate_about_u(NP,gamma,u)
+Ellipsoid.rotate_about_u(gamma,u)
 
 
-# Rotate about x-axis, rotated points now stored in [xyz]r1
-# Perform rotation on individual points about X-axis
-#u=np.array([1.,0.,0.])
-#for i in range(3*NT):
-#    (TP[i,0],TP[i,1],TP[i,2]) = rotate_point_about_u(-1.*alpha,[TP[i,0],TP[i,1],TP[i,2]],u)
-
-
-# Rotate about object's x-axis, rotated points now stored in [xyz]r2
-# Perform rotation on individual points about u
-#u=np.array([math.cos(alpha),(-1)*math.sin(alpha),0.])
-#for i in range(3*NT):
-#    (TP[i,0],TP[i,1],TP[i,2]) = rotate_point_about_u(-1.*beta,[TP[i,0],TP[i,1],TP[i,2]],u);
-
-       
-# Rotate about object's x-axis, rotated points now stored in [xyz]r2
-# Perform rotation on individual points about u
-#u=np.array([math.sin(alpha)*math.cos(beta),
-#            math.cos(alpha)*math.cos(beta),
-#           (-1.)*math.sin(beta)*math.cos(alpha)])
-#for i in range(3*NT):
-#    (TP[i,0],TP[i,1],TP[i,2]) = rotate_point_about_u(1.*gamma,[TP[i,0],TP[i,1],TP[i,2]],u);
-                
-     
      
 mesh = Collada()
 effect = material.Effect("effect0", [], "phong", diffuse=(1,0,0), specular=(0,1,0))
@@ -203,8 +128,8 @@ mesh.effects.append(effect)
 mesh.materials.append(mat)
     
     
-vert_src = source.FloatSource("triverts-array", np.array(TP), ('X', 'Y', 'Z'))
-normal_src = source.FloatSource("trinormals-array", np.array(NP), ('X', 'Y', 'Z'))
+vert_src = source.FloatSource("triverts-array", np.array(Ellipsoid.TP), ('X', 'Y', 'Z'))
+normal_src = source.FloatSource("trinormals-array", np.array(Ellipsoid.NP), ('X', 'Y', 'Z'))
 
 geom = geometry.Geometry(mesh, "geometry0", "mytri", [vert_src, normal_src])
 
@@ -214,7 +139,7 @@ input_list.addInput(0, 'VERTEX', "#triverts-array")
 input_list.addInput(1, 'NORMAL', "#trinormals-array")
 
 
-triset = geom.createTriangleSet(indices, input_list, "materialref")
+triset = geom.createTriangleSet(Ellipsoid.indices, input_list, "materialref")
 geom.primitives.append(triset)
 mesh.geometries.append(geom)
 
